12cbb1e4ddade6887edc5a9591f7cf10991efb63
Stefanel04 <148084814+Stefanel04@users.noreply.github.com>
2024-01-15 18:09:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-function scrollToElement(elementSelector, instance = 0, offset = 0){
+function scrollToElement(elementSelector, instance = 0, offset = 0) {
     const elements = document.querySelectorAll(elementSelector);
-    if (elements.lenght > instance) {
-        elements[instance].scrollIntoView({ behavior: 'smooth' block: 'start', inline: 'nearest', offsetTop: offset});
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest', offsetTop: offset });
     }
 }
 
-const link1 = document.getElementById("Link1")
-const link2 = document.getElementById("Link2")
-const link3 = document.getElementById("Link3")
+const link1 = document.getElementById("Link1");
+const link2 = document.getElementById("Link2");
+const link3 = document.getElementById("Link3");
 
 link1.addEventListener('click', () => {
-    scrollToElement('.header' ,0 ,50)
+    scrollToElement('.header', 0, 0);
 });
+
+link2.addEventListener('click', () => {
+    scrollToElement('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+});
+
+document.getElementById('contactlink').addEventListener('click', function() {
+    document.getElementById('contactModal').style.display = 'block';
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('contactModal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('contactModal')) {
+        document.getElementById('contactModal').style.display = 'none';
+    }
+});
+
+
